203d08e909c4a0ff72ec8d5f0b055cdd7b206bab
swaeliu <juliahul@andrew.cmu.edu>
2024-09-05 22:57:52 -0400

    Fixed all bugs and gobal variable issues.

--- src/admin/advanced/events.js
+++ src/admin/advanced/events.js
@@ -1,24 +1,29 @@
 'use strict';
-define('admin/advanced/events', ['bootbox', 'alerts', 'autocomplete'], 
+define('admin/advanced/events', ['bootbox', 'alerts', 'autocomplete'],
 function (bootbox, alerts, autocomplete) {
+	function deleted(err){
+		if (err) {
+			return alerts.error(err);
+		}
+		$('.events-list').empty();
+
+	}
+	function hello() {
+		bootbox.confirm('[[admin/advanced/events:confirm-delete-all-events]]', (confirm) => {		
+			
+
+			if (confirm) {
+				socket.emit('admin.deleteAllEvents', deleted(err));
+			}
+		});
+		return false;
+	}
+	
 	const Events = {};
 	Events.init = 
 function () {
-		$('[data-action="clear"]').on('click', 
-function () {
-			bootbox.confirm('[[admin/advanced/events:confirm-delete-all-events]]', (confirm) 
-=> {		function (err) {
-	if (err) {
-		return alerts.error(err);
-	}
-	$('.events-list').empty();
-}
-				if (confirm) {
-					socket.emit('admin.deleteAllEvents', err);
-				}
-			});
-			return false;
-		});
+		$('[data-action="clear"]').on('click', hello()
+);
 		$('.delete-event').on('click', function () {
 			const $parentEl = $(this).parents('[data-eid]');
 			const eid = $parentEl.attr('data-eid');
