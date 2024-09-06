036da63eadd5e2d28ed136180caac50a305271a7
swaeliu <juliahul@andrew.cmu.edu>
2024-09-05 22:59:53 -0400

    fixed all bugs related to global variables

--- src/admin/advanced/events.js
+++ src/admin/advanced/events.js
@@ -1,39 +1,43 @@
+/* global define */
+/* global $ */
+/* global socket */
+/* global ajaxify */
+/* global err */
+
 'use strict';
-define('admin/advanced/events', ['bootbox', 'alerts', 'autocomplete'],
-function (bootbox, alerts, autocomplete) {
-	function deleted(err){
+
+define('admin/advanced/events', ['bootbox', 'alerts', 'autocomplete'], (bootbox, alerts, autocomplete) => {
+	function deleted(err) {
 		if (err) {
 			return alerts.error(err);
 		}
 		$('.events-list').empty();
-
 	}
-	function hello() {
-		bootbox.confirm('[[admin/advanced/events:confirm-delete-all-events]]', (confirm) => {		
-			
 
+	function hello() {
+		bootbox.confirm('[[admin/advanced/events:confirm-delete-all-events]]', (confirm) => {
 			if (confirm) {
 				socket.emit('admin.deleteAllEvents', deleted(err));
 			}
 		});
 		return false;
 	}
-	
+
 	const Events = {};
-	Events.init = 
-function () {
-		$('[data-action="clear"]').on('click', hello()
-);
+	Events.init = function () {
+		$('[data-action="clear"]').on('click', hello());
+
 		$('.delete-event').on('click', function () {
 			const $parentEl = $(this).parents('[data-eid]');
 			const eid = $parentEl.attr('data-eid');
-			socket.emit('admin.deleteEvents', [eid], function (err) {
+			socket.emit('admin.deleteEvents', [eid], (err) => {
 				if (err) {
 					return alerts.error(err);
 				}
 				$parentEl.remove();
 			});
 		});
+
 		$('#user-group-select').on('change', function () {
 			const val = $(this).val();
 			$('#username').toggleClass('hidden', val !== 'username');
@@ -45,15 +49,18 @@ function () {
 				$('#group').val('');
 			}
 		});
+
 		autocomplete.user($('#username'));
 		autocomplete.group($('#group'));
 		$('#apply').on('click', Events.refresh);
 	};
+
 	Events.refresh = function (event) {
 		event.preventDefault();
 		const $formEl = $('#filters');
-		ajaxify.go('admin/advanced/events?' + $formEl.serialize());
+		ajaxify.go(`admin/advanced/events?${$formEl.serialize()}`);
 	};
+
 	return Events;
 });
 
